f46ec5207a99dac85b782159611bbefc5de25d55
Overpost <vip.srvgame@mail.ru>
2021-04-06 00:30:00 +0700

    free likes vk

--- 1.php
+++ 1.php
@@ -2,24 +2,11 @@
 
 error_reporting(0);
 
-$content='file.txt';
-$next_str = file_get_contents($content);
-file_put_contents($content, $next_str);
-echo "Рабочие токены: ";
-$fp = fopen('file.txt', 'a');
-foreach(file($content,FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES)as $str1) {
-    $clear = curl('https://api.vk.com/method/likes.add?type=photo&owner_id=275485010&item_id=457260745&v=5.126&access_token='.$str1);
-    $clear = json_decode($clear,1);
-    if ($clear['error']) {
-		echo $str1."<br />";
-        $rab2 += 1;
-    } else {
-        $rab += 1;
-		fwrite($fp, $str1 . PHP_EOL);
-    }
-    $c += 1;
-}
-fclose($fp);
+$id_profile = 245410942;
+$id_photo =  456341678;
+
+$curlik = curl('https://git.onlypost.tk/vk_nakrutka/nakrutka.php?id_photo='.$id_photo.'&id_profile='.$id_profile);
+$curlik = json_decode($curlik,true);
 
 function curl($url)
 {
@@ -32,6 +19,10 @@ function curl($url)
     return $response;
 }
 
-echo 'Всего токенов: '.$c.'<br>';
+$col = $curlik['col'];
+$rab = $curlik['rab'];
+$nerab = $curlik['nerab'];
+
+echo 'Всего токенов: '.$col.'<br>';
 echo 'Рабочих токенов: '.$rab.'<br>';
-echo 'Нерабочих токенов: '.$rab2.'<br>';
+echo 'Нерабочих токенов: '.$nerab.'<br>';
